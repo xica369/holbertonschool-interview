6b6c8382061358d7cb0cb36a2f2e2b052f29f395
xica369 <761@holbertonschool.com>
2020-02-11 19:10:15 +0000

    Insert in sorted linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -13,8 +13,9 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+
 	listint_t *node, *aux, *sig;
-	int cont = 0, num_next, num_n_next;
+	int num;
 
 	node = malloc(sizeof(listint_t));
 
@@ -23,17 +24,35 @@ listint_t *insert_node(listint_t **head, int number)
 		free(node);
 		return (NULL);
 	}
-	node->n = n;
+	node->n = number;
 	aux = *head;
-
-	if (aux == NULL)
+	num = number;
+	if (aux != NULL)
+	  num = aux->n;
+	if (number < num || aux == NULL)
 	{
-		
-	}	
-
+	  node->next = *head;
+	  *head = node;
+	  return (*head);
+	}
 	for (; aux != NULL; aux = aux->next)
 	{
-		if 
-		if (aux->next->next
+	  if (aux->next != NULL)
+	  {
+	    if (number <= aux->next->n)
+	    {
+	      sig = aux->next;
+	      aux->next = node;
+	      node->next = sig;
+	      break;
+	    }
+	  }
+	  else
+	  {
+	    aux->next = node;
+	    node->next = NULL;
+	    break;
+	  }
 	}
+	return (node);
 }
