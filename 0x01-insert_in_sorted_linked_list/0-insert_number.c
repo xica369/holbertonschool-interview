71416105a09f42a7354f2796b3e274c135a936eb
xica369 <761@holbertonschool.com>
2020-02-11 19:19:22 +0000

    betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,22 +3,19 @@
 #include <string.h>
 #include "lists.h"
 /**
- **insert_nodeint_at_index - inserts a new node at a given position
+ **insert_node - inserts a new node at a given position
  *
  *@head: pointer to head nodo to list
- *@idx: index of the list where the new node should be added
  *@number: element to save in n
  *
  *Return: address of the new node or null if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-
 	listint_t *node, *aux, *sig;
 	int num;
 
 	node = malloc(sizeof(listint_t));
-
 	if (node == NULL)
 	{
 		free(node);
@@ -28,31 +25,27 @@ listint_t *insert_node(listint_t **head, int number)
 	aux = *head;
 	num = number;
 	if (aux != NULL)
-	  num = aux->n;
+		num = aux->n;
 	if (number < num || aux == NULL)
 	{
-	  node->next = *head;
-	  *head = node;
-	  return (*head);
+		node->next = *head;
+		*head = node;
+		return (*head);
 	}
 	for (; aux != NULL; aux = aux->next)
 	{
-	  if (aux->next != NULL)
-	  {
-	    if (number <= aux->next->n)
-	    {
-	      sig = aux->next;
-	      aux->next = node;
-	      node->next = sig;
-	      break;
-	    }
-	  }
-	  else
-	  {
-	    aux->next = node;
-	    node->next = NULL;
-	    break;
-	  }
+		if (aux->next != NULL)
+		{
+			if (number <= aux->next->n)
+			{
+				sig = aux->next;
+				aux->next = node;
+				node->next = sig;
+				return (node);
+			}
+		}
 	}
+	aux->next = node;
+	node->next = NULL;
 	return (node);
 }
