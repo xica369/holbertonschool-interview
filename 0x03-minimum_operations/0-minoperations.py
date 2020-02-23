28268c21f9bde8c0e9f2bf40dc852032adef9251
xica369 <761@holbertonschool.com>
2020-02-23 16:01:33 +0000

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Operaciones mínimas"""
+
+
+def minOperations(n):
+    """Returns an integer
+    If n is impossible to achieve, return 0"""
+
+    if not isinstance(n, int):
+        return 0
+
+    if n < 1 or n is None:
+        return 0
+
+    if n == 1:
+        return 0
+
+    if n % 2 != 0:
+        return n + 1
+
+    copy = 1
+    paste = 1
+    h = 2
+    add = 2
+
+    while h < n:
+        if n == h * 2 or n >= h * 4:
+            copy = copy + 1
+            add = h
+
+        paste = paste + 1
+        h = h + add
+
+    return copy + paste
