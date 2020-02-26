f411383a98361fef9fd1a6bb8fd04c099fbaeb88
xica369 <761@holbertonschool.com>
2020-02-26 01:51:40 +0000

    go back

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -13,13 +13,13 @@ def minOperations(n):
     if n <= 1 or n is None:
         return 0
 
+    if n % 2 != 0:
+        return n
+
     copy = 1
     paste = 1
     h = 2
     add = 2
-
-    if n % 2 != 0:
-        copy = 2
 
     while h < n:
         if n == h * 2 or n >= h * 4:
