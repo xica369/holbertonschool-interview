7149fe9b38276808e66ecff8c0099c57b3f3abdd
xica369 <761@holbertonschool.com>
2020-03-18 21:54:24 +0000

    update except

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -33,6 +33,8 @@ try:
             codes_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
                           "403": 0, "404": 0, "405": 0, "500": 0}
 
+except:
+    pass
 
 finally:
     print("File size: {}".format(size))
