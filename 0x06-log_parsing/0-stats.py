5bb8521c27eb5a747acc0b7c33e8d77670981bcc
xica369 <761@holbertonschool.com>
2020-03-18 22:23:55 +0000

    stats again

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,26 +13,28 @@ try:
     for line in sys.stdin:
         split = line.split()
         split = split[::-1]
-        count += 1
 
-        if count <= 10:
+        if len(split) == 9:
+            count += 1
 
-            size += int(split[0])
-            code = split[1]
+            if count <= 10:
 
-            if code in codes_dict.keys():
-                codes_dict[code] += 1
+                size += int(split[0])
+                code = split[1]
 
-        if count == 10:
-            print("File size: {}".format(size))
-            for key, val in sorted(codes_dict.items()):
-                if val != 0:
-                    print("{}: {}".format(key, val))
+                if code in codes_dict.keys():
+                    codes_dict[code] += 1
 
-            count = 0
-            size = 0
-            codes_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
-                          "403": 0, "404": 0, "405": 0, "500": 0}
+            if count == 10:
+                print("File size: {}".format(size))
+                for key, val in sorted(codes_dict.items()):
+                    if val != 0:
+                        print("{}: {}".format(key, val))
+
+                count = 0
+                size = 0
+                codes_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
+                              "403": 0, "404": 0, "405": 0, "500": 0}
 
 except Exception:
     pass
