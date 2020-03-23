a0d8aeedaf9747a213749f49bf80777e95be3ea6
xica369 <761@holbertonschool.com>
2020-03-23 02:56:34 +0000

    Linked list cycle, again

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -13,9 +13,15 @@ int check_cycle(listint_t *list)
 	aux1 = list;
 	aux2 = list->next->next;
 
+	if (list == NULL)
+		return (0);
+
+	if (list->next == NULL)
+		return (0);
+
 	for (; aux2 != NULL; aux2 = aux2->next->next, aux1 = aux1->next)
 	{
-		if (aux1 == aux2)
+		if (aux2->next == aux1)
 			return (1);
 	}
 	return (0);
