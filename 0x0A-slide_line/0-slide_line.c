92ff78ec4ffde94ba6c34392977403dd7736acbd
xica369 <761@holbertonschool.com>
2020-04-12 20:25:40 +0000

    change while to for

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -12,44 +12,44 @@
 
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t cont1 = 0, cont2 = 1, tem1 = 0, tem2, aux1 = 0, aux2, flag = 0;
+	size_t pos1 = 0, pos2 = 1, temp1, temp2, aux1, flag = 0;
 
 	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
 		return (0);
 	if (direction == SLIDE_RIGHT)
-	{       cont1 = size - 1;
-		cont2 = size - 2; }
-	while (tem1 < size)
-	{       tem2 = aux1 + 1;
-		aux2 = cont2;
+	{       pos1 = size - 1;
+		pos2 = size - 2; }
+	for (temp1 = 0; temp1 < size; temp1++)
+	{       aux1 = pos2;
 		flag = 0;
-		while (tem2 < size)
+		for (temp2 = temp1 + 1; temp2 < size; temp2++)
 		{
-			if (line[cont1] != 0 && line[cont2] == line[cont1])
-			{	line[cont1] = line[cont1] * 2;
-				line[cont2] = 0;
+			if (line[pos1] != 0 && line[pos2] == line[pos1])
+			{	line[pos1] = line[pos1] * 2;
+				line[pos2] = 0;
 				break; }
-			if (line[cont1] == 0 && line[cont2] != 0)
-			{	line[cont1] = line[cont2];
-				line[cont2] = 0;
+
+			if (line[pos1] == 0 && line[pos2] != 0)
+			{	line[pos1] = line[pos2];
+				line[pos2] = 0;
 				flag = 1;
-				cont2 = aux2;
-				tem1--;
-				aux1--;
+				pos2 = aux1;
+				temp1--;
 				break; }
-			if (line[cont2] != 0)
+
+			if (line[pos2] != 0)
 				break;
-			direction == SLIDE_LEFT ? cont2++ : cont2--;
-			tem2++; }
-		if (line[cont1] == 0)
+
+			direction == SLIDE_LEFT ? pos2++ : pos2--;
+		}
+		if (line[pos1] == 0)
 			break;
 		if (direction == SLIDE_LEFT && flag == 0)
-		{	cont1++;
-			cont2 = cont1 + 1; }
+		{	pos1++;
+			pos2 = pos1 + 1; }
 		if (direction == SLIDE_RIGHT && flag == 0)
-		{	cont1--;
-			cont2 = cont1 - 1; }
-		tem1++;
-		aux1++; }
+		{	pos1--;
+			pos2 = pos1 - 1; }
+	}
 	return (1);
 }
