ba47bf31dced8b87bd7edbdbd7b6c218739a33f7
xica369 <761@holbertonschool.com>
2020-04-12 03:57:32 +0000

    2048 (single line)

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,55 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - function that slides and merges an array of integers
+ *
+ * @line: array of ints containing size elements, that must be slidued & merged
+ * @size: size elements
+ * @direction: can be either SLIDE_LEFT or SLIDE_RIGHT
+ *
+ * Return: 1 upon success, or 0 upon failure
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+	size_t cont1 = 0, cont2 = 1, temp1 = 0, temp2, aux1 = 0, aux2, flag = 0;
+
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+		return (0);
+	if (direction == SLIDE_RIGHT)
+	{	cont1 = size - 1;
+		cont2 = size - 2; }
+	while (temp1 < size)
+	{	temp2 = aux1 + 1;
+		aux2 = cont2;
+		flag = 0;
+		while (temp2 <= size)
+		{
+			if (line[cont1] != 0 && line[cont2] == line[cont1])
+			{	line[cont1] = line[cont1] * 2;
+				line[cont2] = 0;
+				break; }
+			if (line[cont1] == 0 && line[cont2] != 0)
+			{	line[cont1] = line[cont2];
+				line[cont2] = 0;
+				flag = 1;
+				cont2 = aux2;
+				aux1--;
+				break; }
+			if (direction == SLIDE_RIGHT)
+				cont2--;
+			if (direction == SLIDE_LEFT)
+				cont2++;
+			temp2++; }
+		if (line[cont1] == 0)
+			break;
+		if (direction == SLIDE_LEFT && flag == 0)
+		{	cont1++;
+			cont2 = cont1 + 1; }
+		if (direction == SLIDE_RIGHT && flag == 0)
+		{	cont1--;
+			cont2 = cont1 - 1; }
+		temp1++;
+		aux1++; }
+	return (1);
+}
---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef _SLIDE_LINE_
+#define _SLIDE_LINE_
+
+#include <stdio.h>
+
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 2
+
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif
