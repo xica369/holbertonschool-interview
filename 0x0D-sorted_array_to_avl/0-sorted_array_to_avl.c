bd9727c667cc64bc20e5ca9a9ac0b5888868e5fd
xica369 <761@holbertonschool.com>
2020-05-19 04:02:51 +0000

    AVL - From sorted array

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,27 +1,6 @@
 #include "binary_trees.h"
 
 /**
- *create_binary_tree - function that builds an AVL tree from an array
- *
- *@array: pointer to the first element of the array to be converted
- *@size: number of element in the array
- *@parent: pointer to parent of the new node
- *
- *Return: pointer to the root node of the created AVL tree, or NULL on failure
- */
-avl_t *create_binary_tree(int *array, size_t size, avl_t *parent)
-{
-	avl_t *node;
-
-	node = malloc(sizeof(avl_t));
-	if (node == NULL)
-		return (NULL);
-
-	if (size == 1):
-		
-}
-
-/**
  *sorted_array_to_avl - function that builds an AVL tree from an array
  *
  *@array: pointer to the first element of the array to be converted
@@ -32,15 +11,48 @@ avl_t *create_binary_tree(int *array, size_t size, avl_t *parent)
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	avl_t *root;
+	avl_t *tree;
 
-	if (size == 0)
+	if (!array || size == 0)
 		return (NULL);
 
-	if (array == 0)
+	tree = fill_avl_tree(NULL, array, 0, size - 1);
+
+	return (tree);
+}
+
+
+/**
+ *fill_avl_tree - function that builds an AVL tree from an array
+ *
+ *@parent: pointer to parent of the new node
+ *@array: pointer to the first element of the array to be converted
+ *@start: index to start
+ *@end: index to end
+ *
+ *Return: pointer to the root node of the created AVL tree, or NULL on failure
+ */
+
+avl_t *fill_avl_tree(avl_t *parent, int *array, size_t start, size_t end)
+{
+	avl_t *node;
+	size_t index;
+
+	node = malloc(sizeof(avl_t));
+	if (!node)
 		return (NULL);
 
-	root = create_binary_tree(array, size, NULL);
+	index = (start + end) / 2;
 
-	return (root);
+	node->n = array[index];
+	node->parent = parent;
+
+
+	if (index != start)
+		node->left = fill_avl_tree(node, array, start, index - 1);
+
+	if (index != end)
+		node->right = fill_avl_tree(node, array, index + 1, end);
+
+	return (node);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -14,15 +14,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
 avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *fill_avl_tree(avl_t *parent, int *array, size_t start, size_t end);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
