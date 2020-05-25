aea6b4fd6ac03d15dfb94ead61e3dff15c8e816a
xica369 <761@holbertonschool.com>
2020-05-25 16:44:44 +0000

    basic structure

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,16 @@
+#include "search.h"
+
+/**
+ * linear_skip - Linear search in a skip list
+ *
+ * @list: pointer to the head of the skip list to search in
+ * @value: is the value to search for
+ *
+ * Return: a pointer on the first node where value is located else NULL
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	if (list == NULL)
+		return (NULL);
+}
---
+++ 0x0E-linear_skip/skiplist/search_algos.h
@@ -0,0 +1,28 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdlib.h>
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+
+#endif /* SEARCH_ALGOS_H */
