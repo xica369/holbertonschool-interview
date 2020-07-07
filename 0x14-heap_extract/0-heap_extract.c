c9ed9c6c62a9aa8a7ff329eb393575e4b36699aa
xica369 <761@holbertonschool.com>
2020-07-07 02:11:04 +0000

    basic structure

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - function that extracts the root node of a Max Binary Heap
+ *
+ * @root: is a double pointer to the root node of the heap
+ *
+ * Return: the value stored in the root node or 0 if the function fails
+ */
+int heap_extract(heap_t **root)
+{
+	if (!root)
+		return (0);
+
+	return (1);
+}
