4d0e1ae82f34525d0d0f38484ab5193a1128a087
xica369 <761@holbertonschool.com>
2020-07-26 21:57:24 +0000

    Find the loop

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - function that finds the loop in a linked list
+ *
+ * @head: pointer to head of the list
+ *
+ * Return: node's address where the loop starts or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *fast, *slow;
+
+	if (!head)
+		return (NULL);
+
+	slow = head;
+	fast = head;
+
+	for (; !slow && !fast && !fast->next->next;
+	     slow = slow->next, fast = fast->next->next)
+	{
+		if (slow == fast && slow == head)
+			return (head);
+
+		if (slow == fast && fast != head)
+		{
+			head = head->next;
+			slow = head;
+			fast = head;
+		}
+	}
+	return (NULL);
+}
---
+++ 0x17-find_the_loop/lists.h
@@ -0,0 +1,25 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *find_listint_loop(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+#endif
