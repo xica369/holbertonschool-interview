8e5e48bb6cf8a9eb0e47126a06330f6ca46250ed
xica369 <761@holbertonschool.com>
2020-07-26 22:04:38 +0000

    update

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -18,7 +18,7 @@ listint_t *find_listint_loop(listint_t *head)
 	slow = head;
 	fast = head;
 
-	for (; !slow && !fast && !fast->next->next;
+	for (; !slow && !fast && !fast->next;
 	     slow = slow->next, fast = fast->next->next)
 	{
 		if (slow == fast && slow == head)
