d950707eea1e386f29b6bca6459725707dd74d00
xica369 <761@holbertonschool.com>
2020-08-19 02:22:23 +0000

    basic structure

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ *merge_sort - sorts an array of integers in ascending order with Merge Sort
+ *
+ *@array: array of integer to sort
+ *@size: size of array
+ *
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	print_array(array, size);
+}
