f991cfd5a921a4f1e098d8e538dabfabb06559ce
xica369 <761@holbertonschool.com>
2020-08-30 22:53:10 +0000

    basic structure

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+/**
+ *add_node_end - Add a new node to the end of a double circular linked list
+ *
+ *@list: the list to modify
+ *@str: the string to copy into the new node
+ *
+ *Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+
+}
+
+/**
+ *add_node_begin - Add a new node to the begin of a double circular linked list
+ *
+ *@list: the list to modify
+ *@str: the string to copy into the new node
+ *
+ *Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+
+}
