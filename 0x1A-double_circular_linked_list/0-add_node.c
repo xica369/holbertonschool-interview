dd2af274e9eac429b7c513243402ef4c3f53dbb2
xica369 <761@holbertonschool.com>
2020-08-30 23:18:23 +0000

    add basic case

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -10,7 +10,21 @@
  */
 List *add_node_end(List **list, char *str)
 {
+	List *new_node;
 
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+
+	if (*list = NULL)
+	{
+		new_node->str = str;
+		new_node->next = new_node->prev = new_node;
+		*list = new_node;
+
+		return (new_node);
+	}
+	
 }
 
 /**
