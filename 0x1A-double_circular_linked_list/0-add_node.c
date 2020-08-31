f1875a2d092eda68db3113e216fdcda4fe3e87ca
xica369 <761@holbertonschool.com>
2020-08-31 21:39:13 +0000

    add validate list

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -12,6 +12,9 @@ List *add_node_end(List **list, char *str)
 {
 	List *new_node, *last_node, *temp;
 
+	if (list == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(List));
 	if (!new_node)
 		return (NULL);
@@ -48,6 +51,9 @@ List *add_node_end(List **list, char *str)
 List *add_node_begin(List **list, char *str)
 {
 	List *new_node, *last_node, *temp;
+
+	if (list == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(List));
 	if (!new_node)
