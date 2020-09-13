6f2b7c79e50aabe7027986fb266169a39027419b
xica369 <761@holbertonschool.com>
2020-09-13 21:42:43 +0000

    fixed limit of for

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -23,20 +23,20 @@ def island_perimeter(grid):
     """
 
     grid_len = len(grid[0])
-    water = [0] * grid_len
+    water = [0] * (grid_len + 2)
     perimeter = 0
 
     # add water above and below
     grid.insert(0, water)
     grid.append(water)
 
-    for row in range(1, len(grid)):
+    for row in range(1, len(grid) - 1):
 
         # add water on the left and right
         grid[row].insert(0, 0)
         grid[row].append(0)
 
-        for col in range(1, grid_len):
+        for col in range(1, grid_len + 1):
             if grid[row][col] == 1:
 
                 # check for water above
