f1197e540b26a013ef400596674871e3b5859a40
xica369 <761@holbertonschool.com>
2020-10-15 02:45:26 +0000

    update

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -10,15 +10,8 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-	int idx;
-
 	if (str == NULL || pattern == NULL)
 		return (0);
 
-	for (idx = 0; str[idx]; idx++)
-	{
-		if (str[idx] == '.' || str[idx] == '*')
-			return (0);
-	}
 	return (1);
 }
