fb21274575f9510d3d4bad50772bc2fd8fc18f4f
xica369 <761@holbertonschool.com>
2020-10-15 02:16:38 +0000

    basic structure

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,18 @@
+#include "regex.h"
+
+/**
+ * regex_match - checks whether a given pattern matches a given string
+ *
+ *@str: is the string to scan
+ *@pattern: is the regular expression
+ *
+ * Return: 1 if the pattern matches the string, or 0 if it doesn't
+ */
+int regex_match(char const *str, char const *pattern)
+{
+
+	if (str == NULL || pattern == NULL)
+		return (0);
+
+	return (1);
+}
