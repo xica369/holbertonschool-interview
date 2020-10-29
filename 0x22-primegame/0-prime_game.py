cf6648bd762adc26d08492cc0d79d5593760898f
xica369 <761@holbertonschool.com>
2020-10-29 02:58:52 +0000

    basic struture

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Prime Game
+"""
+
+
+def isWinner(x, nums):
+    """
+    - x is the number of rounds
+    - nums is an array of n
+
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    """
+
+    return None
